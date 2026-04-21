464ff4c9a42908564bc11cd87d184ad494ecb159
Yaazhini0 <yaazhini.n@gmail.com>
2026-04-21 12:59:35 +0530

    UC3

---
+++ App/src/main/java/UC3.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class UC3 {
+    public static void main(String[] args){
+        int slot = getUserSlot();
+        System.out.println("Slot entered: "+slot);
+    }
+    static int getUserSlot(){
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter slot number (1-9): ");
+        int slot = inp.nextInt();
+        return slot;
+    }
+}
